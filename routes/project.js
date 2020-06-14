38bbb614535b7326303cbdb7f4021bc8b6e1e915
ziziw <ziwangchen@hotmail.com>
2020-06-14 15:57:35 -0400

    mostly done. need to add comments and change styles

--- routes/project.js
+++ routes/project.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json');
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
   const {id} = req.params;
-  const project = projects[id];
-  res.render('project', project);
+  if(projects[id]){
+    const project = projects[id];
+    res.render('project', project);
+  } else {
+    next();
+  }
 });
 
 module.exports = router;
