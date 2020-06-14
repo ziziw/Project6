b7ca7001afe3ca7c9dca475b4349982ea5ba6046
ziziw <ziwangchen@hotmail.com>
2020-06-14 03:27:40 -0400

    step 7 complete

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ app.use(mainRoute);
 app.use('/about', aboutRoute);
 app.use('/project', projectRoute);
 
+app.use((req, res, next) => {
+  const err = new Error('Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
